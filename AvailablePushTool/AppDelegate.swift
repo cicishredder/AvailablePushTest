471659d558d51c0873487878228482ba59fe76cf
duyiming <duyiming@didiglobal.com>
2020-09-24 14:56:00 +0800

    Initial Commit

---
+++ AvailablePushTool/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AvailablePushTool
+//
+//  Created by didi on 2020/9/24.
+//  Copyright © 2020 soviet power supreme. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ AvailablePushTool/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AvailablePushTool
+//
+//  Created by didi on 2020/9/24.
+//  Copyright © 2020 soviet power supreme. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
